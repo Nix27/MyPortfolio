d3a3b127419c6098ad339fa038329f9859a1d75e
Nix27 <nikolazecic2468@gmail.com>
2024-01-11 15:30:50 +0100

    Improved skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,10 +4,12 @@ import Accordion from './Accordion'
 import { skillsSection } from '../content'
 
 const Skills = () => (
-  <section id='skills' className='flex flex-col mt-14'>
-    <SectionTitle title={skillsSection.title} />
+  <section id='skills' className='flex flex-col lg:flex-row lg:justify-center mt-14'>
+    <div className='lg:w-[50%] lg:flex lg:items-center lg:justify-center'>
+      <SectionTitle title={skillsSection.title} />
+    </div>
 
-    <div className='flex flex-col gap-x-0 sm:gap-x-14 gap-y-3 mx-10 lg:mx-20 mt-5'>
+    <div className='flex flex-col gap-x-0 sm:gap-x-14 gap-y-3 mx-10 lg:mx-20 mt-5 lg:w-[40%]'>
       {skillsSection.skills.map(skillCategory => (
         <Accordion key={skillCategory.id} {...skillCategory} />
       ))}
